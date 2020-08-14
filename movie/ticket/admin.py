fc4c1af93590176411383f2360bdebd784b2cf02
yusheng0104 <40576387+yusheng0104@users.noreply.github.com>
2020-08-14 09:33:09 -0400

    admin.py added

---
+++ movie/ticket/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Cinima, Movies, Coupon
+# Register your models here.
+
+admin.site.register(Cinima)
+admin.site.register(Movies)
+admin.site.register(Coupon)
